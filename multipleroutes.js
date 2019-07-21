093e6b1f8b39f1c4454431756f0e9a1a7766dfbc
Manik Hosen <rahathossenmanik@gmail.com>
2019-07-21 22:39:56 +0600

    Routing

--- multipleroutes.js
+++ multipleroutes.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var app = express();
-app.get('/hello', function(req, res){
+app.get('/get', function(req, res){
    res.send("Hello World!");
 });
-app.post('/hello', function(req, res){
+app.post('/post', function(req, res){
    res.send("You just called the post method at '/hello'!\n");
 });
 app.listen(4000);
